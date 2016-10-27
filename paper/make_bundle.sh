7aa593533ee3100eb463ce6b8560ed092e4a11f3
Seb James <seb.james@sheffield.ac.uk>
2016-10-27 15:15:00 +0100

    Rename figs according to PLOS ONE guidelines. Change all fonts to Arial. Save files in tiff format

--- paper/make_bundle.sh
+++ paper/make_bundle.sh
@@ -2,19 +2,26 @@
 
 # Make up the zip file for Plos One submission
 
-FIGLIST=`grep includegraph P1_Draft.tex | grep -v PLOS-submission| awk -F '{' '{print \$2}'| awk -F '}' '{print \$1}'`
-echo 'FIGLIST is '${FIGLIST}
+#FIGLIST=`grep includegraph P1_Draft.tex | grep -v PLOS-submission| awk -F '{' '{print \$2}'| awk -F '}' '{print \$1}'`
+
+FIGLIST=''
+XSTART=1
+XEND=10
+for i in `seq ${XSTART} ${XEND}`; do
+    FIGLIST="${FIGLIST} figures/Fig${i}.tiff"
+done
 
 cp ../analysis/Anova.pdf .
 
 rm -f plosone_submission.zip
 
 zip plosone_submission.zip \
-P1_Draft.tex \
+cover_letter.pdf \
+P1_Draft.pdf \
+${FIGLIST} \
 omit_reasons.pdf \
 Anova.pdf \
-cover_letter.pdf \
 striking_image.jpg \
-${FIGLIST}
+P1_Draft.tex
 
 rm -f Anova.pdf
