b823be49c23cece767b3f20eaafd5e2fe1a40251
Seb James <seb.james@sheffield.ac.uk>
2016-10-27 12:17:29 +0100

    Adds missing non-eps figures, plus couple of other fixes

--- paper/make_bundle.sh
+++ paper/make_bundle.sh
@@ -2,12 +2,19 @@
 
 # Make up the zip file for Plos One submission
 
-FIGLIST=`grep eps P1_Draft.tex |grep includegraph | grep -v PLOS-submission| awk -F '{' '{print \$2}'| awk -F '}' '{print \$1}'`
+FIGLIST=`grep includegraph P1_Draft.tex | grep -v PLOS-submission| awk -F '{' '{print \$2}'| awk -F '}' '{print \$1}'`
 echo 'FIGLIST is '${FIGLIST}
+
+cp ../analysis/Anova.pdf .
+
+rm -f plosone_submission.zip
 
 zip plosone_submission.zip \
 P1_Draft.tex \
 omit_reasons.pdf \
-../analysis/Anova.pdf \
+Anova.pdf \
 cover_letter.pdf \
+striking_image.jpg \
 ${FIGLIST}
+
+rm -f Anova.pdf
