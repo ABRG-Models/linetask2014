657e1ba8bab442a0dcb019473b92e4dcd8a65542
Seb James <seb.james@sheffield.ac.uk>
2017-01-23 09:56:01 +0000

    Final layout of code

--- analysis/Bootstrap_all_twosamples.r
+++ analysis/Bootstrap_all_twosamples.r
@@ -1,39 +1,8 @@
 
-set.seed(1974201701)
 
-# Mouse data, for comparison of methods
+# Efron & Tibshirani example mouse data, for comparison of methods.
 mouse.treatment=c(94,197,16,38,99,141,23)     # zdata
 mouse.control=c(52,104,146,10,51,30,40,27,46) # ydata
-
-# This is the number of resamples to do for the bootstrap
-lat.n <- length(ndall)
-
-# Colour scheme
-lat.ndallcol <- "black"
-lat.sdallcol <- "steelblue"
-lat.adallcol <- "red"
-
-lat.ymin <- 0
-lat.ymax <- 0.44
-lat.xmin <- 290
-lat.xmax <- 390
-lat.legx <- 300
-lat.legy <- 0.45
-
-# Hand coded approach to computing bootstrap estimate of the mean
-# difference between two samples. See p 89 of Efron for the two sample
-# problem description
-#
-#lat.ndsd.diffs <- numeric(lat.n) # numeric vector lat.n long
-#for(i in 1:lat.n) {
-#    this.samp1 <- ndall[ sample(length(ndall), length(ndall), replace=TRUE) ]
-#    this.samp2 <- sdall[ sample(length(sdall), length(sdall), replace=TRUE) ]
-#    lat.ndsd.diffs[i] <- mean(this.samp2) - mean(this.samp1)
-#}
-#ndsd.std.err = sqrt(var(lat.ndsd.diffs))
-#ndsd.mean = mean(lat.ndsd.diffs)
-#print (paste("ND SD mean difference:",ndsd.mean))
-#print (paste("ND SD difference standard error estimate:",ndsd.std.err))
 
 # Calculates an estimate of the standard error for the difference
 # between two distributions. Follows section 8.3 of Efron &
@@ -60,25 +29,12 @@ b.diffste <- function(zdata, ydata, B) {
 
 mouseres0 <- b.diffste (mouse.treatment, mouse.control, 1400)
 
-ndsd <- b.diffste(sdall, ndall, 1024)
-print (sprintf("SD ND difference is %f, standard error estimate: %f", ndsd$meandiff, ndsd$stderr))
-#ndsd$df = density(ndsd$diffs)
-#plot (ndsd$df)
-
-ndad <- b.diffste(adall, ndall, 1024)
-print (sprintf("AD ND difference is %f, standard error estimate: %f", ndad$meandiff, ndad$stderr))
-
-sdad <- b.diffste(adall, sdall, 1024)
-print (sprintf("AD SD difference is %f, standard error estimate: %f", sdad$meandiff, sdad$stderr))
-#sdad$df = density(sdad$diffs, n=1024)
-#plot (sdad$df)
-
 # Implements a bootstrapped hypothesis test to get a t-statistic. This
 # follows the algorithm 16.1 described on p 221 of Efron & Tibshirani.
 # data1 and data2 are the two datasets. B is the number of bootstrap
 # replications to make to produce ystar and zstar. data2 is treatment,
 # data1 is control.
-b.test <- function(zdata, ydata, B) {
+b.ttest <- function(zdata, ydata, B) {
     xstar = c(ydata,zdata) # combine the data as if they were6 drawn from a single distribution
     tobs = mean(zdata) - mean(ydata) # observed value of the statistic (difference of means)
     #print (paste("length ydata: ", length(ydata)))
@@ -94,7 +50,7 @@ b.test <- function(zdata, ydata, B) {
     list(asl=asl, minasl=minasl)
 }
 
-# Utility function to display results
+# Utility function to display Achieved Significance Level (ASL) result.
 b.showsiglev <- function (asl, tag) {
     if (asl$asl == 0) {
         msg <- sprintf ("Achieved significance level for %s < %f", tag, asl$minasl)
@@ -105,26 +61,12 @@ b.showsiglev <- function (asl, tag) {
 }
 
 # Reproduce result of algo 16.1 for the mouse data:
-mouseres1 <- b.test(mouse.treatment, mouse.control, 1000)
-
-print ("Algo 16.1")
-
-# ASL < 1/4e6 (2.5e-7) with B=4e6
-ndsd_t <- b.test(sdall, ndall, 1000)
-b.showsiglev (ndsd_t, "SD vs ND")
-
-# ASL < 1/4e6 (2.5e-7)
-ndad_t <- b.test(adall, ndall, 1000)
-b.showsiglev (ndad_t, "AD vs ND")
-
-# With B=1000000, get asl=0.000016
-sdad_t <- b.test(adall, sdall, 1000)
-b.showsiglev (sdad_t, "AD vs SD")
+mouseres1 <- b.ttest(mouse.treatment, mouse.control, 1000)
 
 # Compute a bootstrapped two sample t statistic as per algorithm 16.2
 # in Efron & Tibshirani.
 # zdata is treatment; ydata is control.
-b.twosampt <- function (zdata, ydata, B) {
+b.studentized_ttest <- function (zdata, ydata, B) {
 
     n <- length(zdata)
     m <- length(ydata)
@@ -173,17 +115,4 @@ b.twosampt <- function (zdata, ydata, B) {
 }
 
 # Reproduce result of Algo 16.2 for the mouse data:
-mouseres2 <- b.twosampt(mouse.treatment, mouse.control, 1000)
-
-print ("Algo 16.2 (Studentized)")
-# ndsd does better than 1e-6 (set B to 1000000)
-ndsd_tst <- b.twosampt(sdall, ndall, 1000)
-b.showsiglev (ndsd_tst, "SD vs ND")
-
-# ndad does better than 3e-7 (set B to 3000000)
-ndad_tst <- b.twosampt(adall, ndall, 1000)
-b.showsiglev (ndad_tst, "AD vs ND")
-
-# sdad produced asl of 0.000012 for B=1000000
-sdad_tst <- b.twosampt(adall, sdall, 1000)
-b.showsiglev (sdad_tst, "AD vs SD")
+mouseres2 <- b.studentized_ttest(mouse.treatment, mouse.control, 1000)
--- analysis/Bootstrap_two.r
+++ analysis/Bootstrap_two.r
@@ -8,8 +8,65 @@
 # following reviewer comments that the analysis was not correct.
 #
 
+set.seed(1974201701)
+
 load(file="all_latencies.rdat")
 
-filesuffix <- "all"
+filesuffix <- "twosamp"
 # To call this, you need to have ndall, sdall and adall in the workspace:
 source('Bootstrap_all_twosamples.r')
+
+#
+# Bootstrap the difference between the means of pairs of distributions:
+#
+print("-----------------------------------------")
+print("Bootstrap analysis of difference of means")
+ndsd <- b.diffste(sdall, ndall, 1024)
+print (sprintf("SD ND difference is %f, standard error estimate: %f", ndsd$meandiff, ndsd$stderr))
+#ndsd$df = density(ndsd$diffs)
+#plot (ndsd$df)
+
+ndad <- b.diffste(adall, ndall, 1024)
+print (sprintf("AD ND difference is %f, standard error estimate: %f", ndad$meandiff, ndad$stderr))
+
+sdad <- b.diffste(adall, sdall, 1024)
+print (sprintf("AD SD difference is %f, standard error estimate: %f", sdad$meandiff, sdad$stderr))
+#sdad$df = density(sdad$diffs, n=1024)
+#plot (sdad$df)
+
+
+#
+# Apply a hypothesis test to obtain the probability that the pairs of
+# distributions are drawn from the same population.
+#
+
+#print ("Algo 16.1")
+
+# ASL < 1/4e6 (2.5e-7) with B=4e6
+#ndsd_t <- b.ttest(sdall, ndall, 1000)
+#b.showsiglev (ndsd_t, "SD vs ND")
+
+# ASL < 1/4e6 (2.5e-7)
+#ndad_t <- b.ttest(adall, ndall, 1000)
+#b.showsiglev (ndad_t, "AD vs ND")
+
+# With B=1000000, get asl=0.000016
+#sdad_t <- b.ttest(adall, sdall, 1000)
+#b.showsiglev (sdad_t, "AD vs SD")
+
+
+print("-----------------------------------------")
+print ("Studentized bootstrapped hypothesis test (Algo 16.2)")
+# ndsd does better than 1e-6 (set B to 1000000)
+ndsd_tst <- b.studentized_ttest(sdall, ndall, 1000)
+b.showsiglev (ndsd_tst, "SD vs ND")
+
+# ndad does better than 3e-7 (set B to 3000000)
+ndad_tst <- b.studentized_ttest(adall, ndall, 1000)
+b.showsiglev (ndad_tst, "AD vs ND")
+
+# sdad produced asl of 0.000012 for B=1000000
+sdad_tst <- b.studentized_ttest(adall, sdall, 1000)
+b.showsiglev (sdad_tst, "AD vs SD")
+
+print("-----------------------------------------")
