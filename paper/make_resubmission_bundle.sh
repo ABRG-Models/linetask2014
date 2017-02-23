a3efa208707a690b2ce1485f56c4c51c25ce4b5d
Seb James <seb.james@sheffield.ac.uk>
2017-02-23 17:02:28 +0000

    The resubmission bundle; a signature for the revisions document and updates for the resubmission bundling script.

--- paper/make_resubmission_bundle.sh
+++ paper/make_resubmission_bundle.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Make up the zip file for Plos One submission
+# Make up the zip file for Plos One re-submission
 
 #FIGLIST=`grep includegraph P1_Draft.tex | grep -v PLOS-submission| awk -F '{' '{print \$2}'| awk -F '}' '{print \$1}'`
 
 FIGLIST=''
 XSTART=1
-XEND=10
+XEND=12
 for i in `seq ${XSTART} ${XEND}`; do
     FIGLIST="${FIGLIST} figures/Fig${i}.tiff"
 done
 
 cp ../analysis/Anova.pdf .
 
-rm -f plosone_submission.zip
+rm -f plosone_resubmission.zip
 
-zip plosone_submission.zip \
-cover_letter.pdf \
+zip plosone_resubmission.zip \
+revisions.pdf \
+P1_Draft_annotated.pdf \
 P1_Draft.pdf \
 ${FIGLIST} \
 omit_reasons.pdf \
